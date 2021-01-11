5bc86a774a8893a4185cede0851e5bc2906dc0f0
areum514 <21500514@handong.edu>
2021-01-11 20:20:48 +0900

    #2.1 Create Room part One

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.views import APIView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework import status
 from .models import Room
-from  .serializers import RoomSerializer
+from .serializers import ReadRoomSerializer, WirteRoomSerializer
 # Create your views here.
 
-class ListRoomView(ListAPIView):
-    queryset=Room.objects.all()
-    serializer_class=RoomSerializer
+@api_view(["GET", "POST"])
+def rooms_view(request):
+    if request.method == "GET":
+        rooms = Room.objects.all()
+        serializer = ReadRoomSerializer(rooms, many=True).data
+        return Response(serializer)
+    elif request.method == "POST":
+        serializer = WirteRoomSerializer(data=request.data)
+        print(dir(serializer))
+        if serializer.is_valid():
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class SeeRoomView(RetrieveAPIView):
     queryset=Room.objects.all()
-    serializer_class=RoomSerializer
+    serializer_class=ReadRoomSerializer
