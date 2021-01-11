d7bd82eafa8fa7c9bd9fdf5080764807ae3e264d
areum514 <21500514@handong.edu>
2021-01-11 18:28:01 +0900

    #1.2 Serializers

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from .models import Room
+from  .serializers import RoomSerializer
 # Create your views here.
 @api_view(['GET'])
 def list_rooms(request):
-    return Response()
+    rooms=Room.objects.all()
+    serialized_rooms=RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
